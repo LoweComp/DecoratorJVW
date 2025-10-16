4e54d6ef1fb37885b3530ed300e1a51b5a1ca081
LoweComp <terci_300@outlook.com>
2025-10-16 13:04:35 -0300

    Primeira gama de testes individuais

---
+++ src/test/java/com/DecoratorJVW/FestaDecoratorTest.java
@@ -0,0 +1,59 @@
+package com.DecoratorJVW;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FestaDecoratorTest {
+    private static final double CUSTO_BASE = 4000.00;
+    private static final double CUSTO_BUFFET = 3800.00;
+    private static final double CUSTO_OPEN_BAR = 2000.00;
+    private static final double CUSTO_BANDA_HORA = 400.00;
+    private static final double CUSTO_DJ_OUTSIDER_HORA = 600.00;
+
+    private static final int HORAS_BANDA = 3;
+    private static final int HORAS_DJ_OUTSIDER = 2;
+
+    // TESTES FESTA BASE + SERVIÇO INDIVIDUAL
+    @Test
+    void testeSolo_FestaBase() {
+        IFesta festa = new FestaBase();
+        assertEquals(CUSTO_BASE, festa.getCusto());
+        assertEquals("Festa Básica com DJ Residente, Segurança e Bar", festa.getDescricao());
+    }
+
+    @Test
+    void testeSolo_BuffetPremium() {
+        IFesta festa = new BuffetPremiumDec(new FestaBase());
+        assertEquals(CUSTO_BASE + CUSTO_BUFFET, festa.getCusto());
+        assertEquals("Festa Básica com DJ Residente, Segurança e Bar, Buffet Premium (Jantar Completo, Mesa de Frio & Aperitivos)", festa.getDescricao());
+    }
+
+    @Test
+    void testeSolo_OpenBar() {
+        IFesta festa = new OpenBarDec(new FestaBase());
+        assertEquals(CUSTO_BASE + CUSTO_OPEN_BAR, festa.getCusto());
+        assertEquals("Festa Básica com DJ Residente, Segurança e Bar, Open Bar de bebidas alcoolicas, refrigerante, água e sucos", festa.getDescricao());
+    }
+
+    @Test
+    void testeSolo_BandaPorHora() {
+        IFesta festa = new BandaDec(new FestaBase(), HORAS_BANDA);
+
+        double custoBandaTotal = CUSTO_BANDA_HORA * HORAS_BANDA;
+        assertEquals(CUSTO_BASE + custoBandaTotal, festa.getCusto());
+
+        String expectedDesc = String.format("Festa Básica com DJ Residente, Segurança e Bar, Banda Sertaneja/Pagode (%d horas)", HORAS_BANDA);
+        assertEquals(expectedDesc, festa.getDescricao());
+    }
+
+    @Test
+    void testeSolo_DJOutsiderPorHora() {
+        IFesta festa = new DJOutsiderDec(new FestaBase(), HORAS_DJ_OUTSIDER);
+
+        double custoDJTotal = CUSTO_DJ_OUTSIDER_HORA * HORAS_DJ_OUTSIDER;
+        assertEquals(CUSTO_BASE + custoDJTotal, festa.getCusto());
+
+        String expectedDesc = String.format("Festa Básica com DJ Residente, Segurança e Bar, DJ LoweZera (%d horas)", HORAS_DJ_OUTSIDER);
+        assertEquals(expectedDesc, festa.getDescricao());
+    }
+}
